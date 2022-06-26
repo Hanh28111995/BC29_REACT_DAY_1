342f545b9b1d4daf40f233216854564e82da2a70
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2022-06-26 10:34:53 +0700

    clear

--- bc29reactjs/src/Components/BaiTapLayoutComponent/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Footer extends Component {
-  render() {
-    return (
-      <div className='text-white py-5'>Copyright © Your Website 2022</div>
-    )
-  }
-}
